e7e2669b1314fa19801743df46720b1e7e5853f8
Simon Rice <simon.rice@massive.co>
2017-01-09 08:10:25 +0000

    Made all the things public, added framework target & Xcode playground

---
+++ Components-iOS/Components-iOS.h
@@ -0,0 +1,19 @@
+//
+//  Components-iOS.h
+//  Components-iOS
+//
+//  Created by Simon Rice on 09/01/2017.
+//  Copyright © 2017 Jahanvi Vyas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Components-iOS.
+FOUNDATION_EXPORT double Components_iOSVersionNumber;
+
+//! Project version string for Components-iOS.
+FOUNDATION_EXPORT const unsigned char Components_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Components_iOS/PublicHeader.h>
+
+
---
+++ ComponentsPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import Components
+import UIKit
+
+let linkButton = LinkButton(frame: CGRect(x: 0, y: 0, width: 100, height: 100))
+linkButton.setTitleColor(.black, for: .normal)
+linkButton.setTitle("Hello", for: .normal)
